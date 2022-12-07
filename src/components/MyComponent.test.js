3692cf58d4a2f8e07d1254d5adc67618ee8594c7
Shandira <shandira.ferguson@gmail.com>
2022-12-06 19:42:59 -0800

    Exercise complete

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -1,9 +1,30 @@
+// @ts-nocheck
 import React from 'react';
 import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
 
-describe("MyComponent", () => {
-  it("should render correctly", () => {
-    const component = shallow(<MyComponent />);
-  });
-});
+describe( "MyComponent", () => {
+  it( "should render correctly", () => {
+    const wrapper = shallow( <MyComponent /> );
+  } );
+  it( "should render initial layout", () => {
+    //when
+    const component = shallow( <MyComponent /> );
+    //then
+    expect( component.getElements() ).toMatchSnapshot();
+  } );
+  it( "should create an entry in component state", () => {
+    //given
+    const component = shallow( <MyComponent /> );
+    const form = component.find( 'input' );
+    //when
+    form.props().onChange( {
+      target: {
+        name: 'myName',
+        value: 'myValue'
+      }
+    } );
+    //then
+    expect( component.state( 'input' ) ).toEqual( 'myValue' );
+  } )
+} );
